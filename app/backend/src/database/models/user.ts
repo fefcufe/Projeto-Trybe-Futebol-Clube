56b10bb368ec8a4f2a6c2ebcc48756a3b57f637b
Fernanda Ferreira <fefcufe@gmail.com>
2022-08-23 12:51:00 -0300

    feat: adiciona migration e model de users

---
+++ app/backend/src/database/models/user.ts
@@ -0,0 +1,42 @@
+import { STRING, INTEGER, Model } from 'sequelize';
+import db from '.'; // importa index.ts dessa pasta (models)
+
+class User extends Model {
+  id!: number;
+  // "!" é para não precisar definir um valor default ja que o sequelize já
+  // o definirá
+  username!: string;
+  role!: string;
+  email!: string;
+  password!: string;
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'users', // nome da tabela
+  underscored: true,
+});
+
+export default User;
